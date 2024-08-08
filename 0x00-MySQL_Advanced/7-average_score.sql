ee01df9ced2c1d4eb5629cde5e3a211cfe285dcb
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-08 22:25:20 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,28 +1,17 @@
 -- Drop the procedure if it already exists
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
--- Change the delimiter to // for procedure definition
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser
 DELIMITER //
-
--- Create the stored procedure
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT)
 BEGIN
-    -- Variable to hold the average score
     DECLARE avg_score FLOAT;
-    
-    -- Calculate the average score for the given user
     SET avg_score = (
         SELECT AVG(score)
         FROM corrections
         WHERE user_id = user_id
     );
-
-    -- Update the average_score field in the users table
     UPDATE users
     SET average_score = IFNULL(avg_score, 0)
     WHERE id = user_id;
 END //
-
--- Reset the delimiter
 DELIMITER ;
