5464dff3888791397b71dba90c8232d81eaad7c3
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-08 22:29:55 +0000

    average

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,17 +1,5 @@
--- Drop the view if it exists
-DROP VIEW IF EXISTS glam_rock_bands;
-
 -- Create a view to list all bands with Glam rock as their main style, ranked by longevity
-CREATE VIEW glam_rock_bands AS
-SELECT 
-    band_name,
-    (2022 - formed) AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    main_style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
-
--- Select from the view to see the results
-SELECT * FROM glam_rock_bands;
+SELECT band_name AS band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
