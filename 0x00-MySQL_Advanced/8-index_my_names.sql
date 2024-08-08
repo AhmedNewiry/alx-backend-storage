05f51fced12cfbbfad7ccf1b1903cb8156e7f0ab
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-08 22:15:37 +0000

    index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,3 @@
--- Drop the index if it already exists
-DROP INDEX IF EXISTS idx_name_first ON names;
-
 -- Create an index on the first letter of the `name` column
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
+CREATE INDEX idx_name_first
+ON names (name(1));
