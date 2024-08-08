0ce71508fa79009a18021220a45215c0167915a1
ahmedNewiry <ahmedabdalfttah@outlook.com>
2024-08-08 22:26:34 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,17 +1,12 @@
 -- Drop the procedure if it already exists
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    SET avg_score = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE user_id = user_id
-    );
-    UPDATE users
-    SET average_score = IFNULL(avg_score, 0)
-    WHERE id = user_id;
-END //
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+//
 DELIMITER ;
